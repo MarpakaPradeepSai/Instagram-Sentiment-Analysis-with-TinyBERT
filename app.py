e8f27a0c53c4078605acf5810fbcd2138aaac429
Marpaka Pradeep Sai <153713502+MarpakaPradeepSai@users.noreply.github.com>
2024-10-16 20:00:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import streamlit as st
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
+import requests
+import os
+
+# Function to download model files from GitHub
+def download_file_from_github(url, local_path):
+    response = requests.get(url)
+    with open(local_path, 'wb') as f:
+        f.write(response.content)
+
+# URLs of your model files on GitHub
+repo_url = 'https://github.com/MarpakaPradeepSai/Instagram-Sentiment-Analysis-with-TinyBERT/raw/main/TinyBERT_model'
+files = ['config.json', 'model.safetensors', 'special_tokens_map.json', 'tokenizer_config.json', 'training_args.bin', 'vocab.txt']
+
+# Download each file
+for file in files:
+    download_file_from_github(f"{repo_url}/{file}", f"./TinyBERT_model/{file}")
 
 # Load tokenizer and model
-model_path = "./TinyBERT_model"
-tokenizer = BertTokenizer.from_pretrained(model_path)
-model = BertForSequenceClassification.from_pretrained(model_path)
+tokenizer = BertTokenizer.from_pretrained('./TinyBERT_model')
+model = BertForSequenceClassification.from_pretrained('./TinyBERT_model')
 
 # Function to predict sentiment
 def predict_sentiment(text):
